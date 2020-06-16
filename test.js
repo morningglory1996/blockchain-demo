d2f446f6865d0b2cb8ecb81f2cd8c47c93088f89
wataru <morningglory1996@outlook.jp>
2020-06-16 16:30:23 +0900

    fix test.js

--- test.js
+++ test.js
@@ -7,9 +7,8 @@ bitcoin.createNewTransaction(2, 'SAM', 'LUCY');
 mining(bitcoin);
 bitcoin.createNewTransaction(3, 'JOHN', 'LISA');
 mining(bitcoin);
-console.log(bitcoin.blockChain[1].transactionData);
-console.log(bitcoin.blockChain[2].transactionData);
-console.log(bitcoin.blockChain[3].transactionData);
+console.log(bitcoin.blockChain);
+
 
 
 
